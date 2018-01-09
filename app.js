91e8839eb4310a69a15535f20dfce3d0aa9a3b92
Worldclassdev <philip.obosi@eng.uniben.edu>
2018-01-10 00:20:36 +0100

    stoill working

--- app.js
+++ app.js
@@ -2,15 +2,14 @@ console.log('starting app.js');
 
 const fs = require('fs');
 const _ = require('lodash');
-//const yargs = require('yargs');
+const yargs = require('yargs'); 
 
 
 const notes = require('./notes');
 
-//const argv = yargs.argv;
-var command = process.argv[2];
-console.log(`Command : ${command}`);
-//console.log (`Yargs, ${argv}`);
+const argv = yargs.argv;
+var command = argv._[0];
+console.log (`Yargs: ${argv}`);
 
 if(command === 'add'){
     notes.addNote(argv.title, argv.body);
--- notes.js
+++ notes.js
@@ -1,7 +1,31 @@
 console.log('Starting notes.js');
 
+const fs = require('fs');
+
+var fetchNotes = () =>{
+    try {
+        return JSON.parse(fs.readFileSync('notes-data.json'));
+    } catch (e){
+        return [];
+    }
+}
+
 var addNote = (title,body) => {
-    console.log('Adding note', title,body);
+    var notes= [];
+    notes = fetchNotes(); 
+    var note = {
+        title,
+        body
+    };
+    for (let savedNote of notes) {
+        if (savedNote.title == note.title){
+                console.log(`Note title exists, Re-attempt saving with a new title`);
+                process.exit();
+        }      
+    }
+    
+    notes.push(note);
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
 };
 
 var getNotes = () => {
